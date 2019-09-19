c3553b7a36c1c2d61e9c109ae6fb683522f032ab
Andreas Smidelöv <Andreas.Smidelov@claremont.se>
2019-09-19 10:26:48 +0200

    Proof of concept

---
+++ src/components/BudgetTracker.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="grey lighten-3 py-10">
+            <div class="d-flex justify-center">
+                <div class="display-4">{{budget}}</div>
+            </div>
+        </div>
+        <div class="my-10">
+            <div class="d-flex justify-center">
+                <v-btn x-large class="my-3" minWidth="200" @click="budget++">ADD</v-btn>
+            </div>
+            <div class="d-flex justify-center">
+                <v-btn x-large class="my-3" minWidth="200" @click="budget--">SUBTRACT</v-btn>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BudgetTracker',
+    data() {
+        return {
+            budget: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
